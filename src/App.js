45b5d6239dcfbbc1a3ba758c4876ee454512d8cf
raaghavvd <raaghavvd@gmail.com>
2020-03-14 02:58:21 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -18,20 +18,87 @@ const arrayOfData=[
     },
 
 ]
+let sorterOrderID
 
+const sortChoices=[
+   
 
+    {
+        'id':'asc',
+        'value':'Order Choices by Ascending'
+    },
+    {
+        'id':'desc',
+        'value':'Order Choices by Descending'
+    }
+]
 class App extends Component {
     constructor(props) {
         super(props);
         // this.label = ;
+        this.state={
+            selectValue:""
+        }
         this.checker=0;
         this.activateLasers=this.activateLasers.bind(this);
         this.activateMultiple=this.activateMultiple.bind(this); 
         this.defaultChange=this.defaultChange.bind(this);
         // this.clearForm=this.clearForm(this);
-        this.sortAlphabets=this.sortAlphabets(this);
+        this.sortAlphabets=this.sortAlphabets.bind(this);
+        this.orderSelection=this.orderSelection.bind(this);
+        this.handleDropDownChange=this.handleDropDownChange.bind(this);
+
+
         // this.upsertValue=this.upsertValue.bind(this);
 
+    }
+    handleDropDownChange(e){
+        console.log(this.sorterSelect)
+        // this.setState({selectValue:e.target.value})
+    }
+    orderSelection(event){
+        console.log("ORDER~!!")
+        console.log(event.target.id)
+
+       sorterOrderID= sortChoices.map((data)=>{
+            if(data.value===event.target.value){
+                return data.id;
+            }
+        });
+        arrayOfData.sort(this.sortAlphabets())   
+        console.log(arrayOfData) 
+
+    }
+    sortAlphabets(value){
+        console.log("SORTER")
+        if(sorterOrderID=="asc"){
+
+            return function(a,b){
+                if(a[value]>b[value]){
+                    return 1
+                }
+                else if(a[value]<b[value]){
+                    return -1
+                }
+        
+        }
+        
+
+        }
+        else{
+            return function(a,b){
+                if(a[value]>b[value]){
+                    return -1
+                }
+                else if(a[value]<b[value]){
+                    return 1
+                }
+
+        }
+
+       
+        
+        }
     }
     activateLasers(value) {
         // console.log(value.target.multiple=true)
@@ -44,26 +111,19 @@ class App extends Component {
             value.target.multiple=false;
         }
         console.log(value)
-        this.handleChange()
+        
     }
-    handleChange = (event) => {
-        // this.setState({
-        //     value: event.target
-        // });
+  
+  
 
-    console.log(this.event.target.value)
-    }
-    sortAlphabets(value){
-        console.log("SORTER")
         // if(this.sorter===1){
         //     arrayOfData.sort((a,b) => {
         //         return a.id > b.id;
         //     });
+            
         //     this.setState({
         //         contacts: itemsArray
         //     })
-        //     arrayOfData.sort(a>b,)
-        //     arrayOfData.sort()
         // }
        
        
@@ -71,7 +131,7 @@ class App extends Component {
         // console.log(arrayOfData)
         // return arrayOfData;
         
-    }
+    
     
     activateMultiple(value){
         console.log("toggle")
@@ -104,6 +164,7 @@ handleButtonClick = () => {
     
 
     upsertValue(selectedValue){
+
         console.log("UPSERT!!!")
         
         let presentVal=false;
@@ -117,6 +178,7 @@ handleButtonClick = () => {
         }
         console.log(arrayOfData)
     }
+    
 
   render() {
         // let arrayOfData = this.props.arrayOfData;
@@ -130,6 +192,15 @@ handleButtonClick = () => {
                     {data.value}
                 </option>
             );
+        let sortOptions= sortChoices.map((data) =>
+        <option 
+            key={data.id}
+            value={data.value}
+        >
+            {data.value}
+        </option>
+    );
+    
       return (
           <form className="container" ref={form => this.form = form}>
               <div className="form-group row">
@@ -164,32 +235,20 @@ handleButtonClick = () => {
               <div className="form-group row">
                   <label for="defaultValue" classsName="col-md-2 col-form-label">Choices</label>
                   <div class="col-md-4">
-                      <select id="inputGetter" class="form-control" onChange={this.activateLasers}>
-                          <option selected>Continent selector</option>
+                      <select id="inputGetter" class="form-control" onChange={this.activateLasers} placeholder= "Select">
+                          {/* <option placeholder>Continent selector</option> */}
                             {options}
 
                       </select>
                   </div>
               </div>
-              <div className="form-group row">
-                  <label for="defaultValue" classsName="col-md-2 col-form-label">Order</label>
-                  <div className="col-md-2">
-                      <label class="form-check-label" for="checker">
-                          <b>Ok</b>
-                      </label>
-                  </div>
-                  <div className="col-md-4">
-                      <input class="form-check-input" type="checkbox" value="" id="sorter" onClick={this.sortAlphabets}/>
-                      Sort the elements by alphabetical order
-                  </div>
-                   
-              </div>
+             
               <div className="form-group row">
                   <label for="defaultValue" classsName="col-sm-2 col-form-label">Order</label>
                   <div class="col-sm-10">
                       <div class="col-sm-6">
-                          <select id="inputState" class="form-control">
-                              <option selected>Display choices in Alphabetical Order</option>
+                          <select id="sorterSelect" class="form-control" onChange={this.orderSelection} value={this.state.selectValue} >
+                              {sortOptions}
                           </select>
                       </div>
                   </div>
